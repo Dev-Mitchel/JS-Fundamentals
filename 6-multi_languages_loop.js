8b967b82a1e3b0badf3804a52b6f805740ac7733
Mitchel Nwachukwu <nwachukwumitchel53@gmail.com>
2025-11-09 23:30:42 -0800

    a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+array =[
+    "C is fun",
+    "Python is cool ",
+    "JavaScript is amazing"
+]
+
+for (let i =0; i < 1; i++) {
+    console.log(array[0])
+     console.log(array[1])
+      console.log(array[2])
+}
+    
