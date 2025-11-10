5bd2e476ce629c60e954fad659fd64b024152cfe
Mitchel Nwachukwu <nwachukwumitchel53@gmail.com>
2025-11-10 00:04:29 -0800

    a script that prints x times 'C is fun'

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+const num = Number(args[0]);
+
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
