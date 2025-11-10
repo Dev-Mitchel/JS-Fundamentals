5229c2938a3e54bedc3308f727197439509ef3c6
Mitchel Nwachukwu <nwachukwumitchel53@gmail.com>
2025-11-10 01:15:23 -0800

    a script that prints the addition of 2 integers

--- 9-add.js
+++ 9-add.js
@@ -1,6 +1,12 @@
 const args = process.argv.slice(2);
-const size = Number(args);
+
 function add(a, b) {
     console.log(a + b);
 }
-add(args[0], args[1]);
+
+// Convert each argument to a number before calling add
+const num1 = Number(args[0]);
+const num2 = Number(args[1]);
+
+add(num1, num2);
+
