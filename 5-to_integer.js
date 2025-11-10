cbcaf0b1fb6bed846186626a73c7650e71087009
Mitchel Nwachukwu <nwachukwumitchel53@gmail.com>
2025-11-09 22:36:51 -0800

    a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,18 @@
 const args = process.argv.slice(2);
 
-if (typeof(args[0]) === "string") {
-    console.log("My number:"+ " " + args[0])
-} else if (args[0]){
-    console.log("Not a number")
+const num = Number(args[0]);  // convert to number
+
+// Understanding isNaN(num)
+
+// isNaN() is a built-in JavaScript function that checks “Is this value Not a Number?”
+
+// So:
+
+// isNaN(10)      // false (because 10 is a number)
+// isNaN('hello') // true  (because 'hello' cannot be turned into a number)
+// isNaN(NaN)     // true
+if (!isNaN(num)) {
+  console.log('My number: ' + num);
+} else {
+  console.log('Not a number');
 }
