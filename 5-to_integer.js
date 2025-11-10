a8b14afef13ae29528a3f216440305105c7ace32
Mitchel Nwachukwu <nwachukwumitchel53@gmail.com>
2025-11-09 18:22:49 -0800

    a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 
 if (typeof(args[0]) === "string") {
-    console.log("My number:" + args[0])
+    console.log("My number:"+args[0])
 } else {
     console.log("Not a number")
 }
